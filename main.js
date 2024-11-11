22bb4a5587f704ce7b10cf105d9e7f1fe34ade5d
Ericzeppe53 <Ericzeppe53@hotmail.com>
2024-11-11 14:08:01 +0100

    Test

---
+++ main.js
@@ -0,0 +1,8 @@
+window.navHover = navHover;
+window.navLeave = navLeave;
+function navHover(){
+    document.getElementById("links").style.display = "";
+}
+function navLeave(){
+    document.getElementById("links").style.display = "none";
+}
